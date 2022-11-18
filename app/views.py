b705c7a42b0f09a6496f484724033f9cdec9b226
Amanda <amandadiassc@gmail.com>
2022-11-18 19:39:11 -0300

    update error messages

--- app/views.py
+++ app/views.py
@@ -28,9 +28,12 @@ def entrar(request):
                     login(request, usuario)
                     messages.add_message(request, messages.SUCCESS, 'Usuário logado com sucesso!')
                     return redirect('mostra_home', pk=paciente.pk)
+            else:
+                messages.add_message(request, messages.INFO, 'Senha digitada errada!')
+                return HttpResponseRedirect('/')
         except User.DoesNotExist:
             messages.add_message(request, messages.INFO, 'Usuário não cadastrado!')
-            return HttpResponseRedirect('/')
+            return HttpResponseRedirect('/')            
     return render(request, 'register/login.html', {'titulo': 'NutriOnline'})
 
 @login_required(login_url='entrar')
